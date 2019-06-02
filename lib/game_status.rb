0c55ddf6b914cfe89013f7e41ed175a7b1ec796c
Sanskar Bansal <sanskarbnsl75@gmail.com>
2019-06-02 07:44:07 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,7 +25,9 @@ def won?(board)
       return true
     end
     flag = win_comb.all?{|s| board[s]=="O"}
-    if flag { return true }
+    if flag
+       return true
+     end
   end
   return false
 end
